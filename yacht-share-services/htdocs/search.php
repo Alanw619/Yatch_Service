d1184ff39c26b3644a388856d9c185c512d92e26
Alanw619 <58181337+Alanw619@users.noreply.github.com>
2021-05-11 11:58:17 +0200

    Add files via upload

---
+++ yacht-share-services/htdocs/search.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha512-SfTiTlX6kk+qitfevl/7LibUOeJWlt9rbyDn92a1DqWOw9vWG2MFoays0sgObmWazO5BQPiFucnnEAjpAB+/Sw==" crossorigin="anonymous" />    
+    <link rel="stylesheet" href="./codyhouse-framework-master/main/assets/css/style.css">
+    <link rel="stylesheet" href="./codyhouse-framework-master/main/assets/css/main.css">
+    <title>Yacht Share Services</title>
+</head>
+
+<body>
+    <?php
+    include '_header.php';
+    ?>
+
+    
+
+    <?php
+    include '_footer.php'
+    ?>
+    <script src="./codyhouse-framework-master/main/assets/js/scripts.js"></script>
+    <script src="./codyhouse-framework-master/main/assets/js/navbar.js"></script>
+</body>
+
+</html>
